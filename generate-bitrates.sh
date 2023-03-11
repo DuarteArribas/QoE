ae08f8fdd8d11ab024cdeb760eb5496796ee6d5f
duarte.a.arribas@gmail.com <duarte.a.arribas@gmail.com>
2023-03-11 18:26:44 +0000

    tou farot desta merda

--- generate-bitrates.sh
+++ generate-bitrates.sh
@@ -1,17 +1,28 @@
-#!/bin/bash
+#!/bin/zsh
+
+mkdir images/JPG
+mkdir images/JPG2000
+mkdir images/AV1
 
 for i in {1..16};do
   # JPEG
-  ffmpeg -i images/${i}.png -q:v 100  images/${i}-1.jpg
-  ffmpeg -i images/${i}.png -q:v 70   images/${i}-2.jpg
-  ffmpeg -i images/${i}.png -q:v 30   images/${i}-3.jpg
-  ffmpeg -i images/${i}.png -q:v 1    images/${i}-4.jpg
-
+  for j in {0..3};do
+    let bitrate=$(($j * (31 / 3) + 1))
+    mkdir images/JPG/bitrate-$((4-$j)) 2> /dev/null
+    ffmpeg -nostdin -y -i images/References/$i.png -pix_fmt rgb24 -q:v $bitrate images/JPG/bitrate-$((4-$j))/$i.jpg
+  done
   # JPEG2000
-  ffmpeg -i images/${i}.png -q:v 100  images/${i}-1.jp2
-  ffmpeg -i images/${i}.png -q:v 70   images/${i}-2.jp2
-  ffmpeg -i images/${i}.png -q:v 30   images/${i}-3.jp2
-  ffmpeg -i images/${i}.png -q:v 1    images/${i}-4.jp2
-
-  # HEVC
+  for j in {0..3};do
+    let bitrate=$(($j * (1000 / 3) + 1))
+    mkdir images/JPG2000/bitrate-$((4-$j)) 2> /dev/null
+    ffmpeg -nostdin -y -i images/References/$i.png -pix_fmt rgb24 -codec jpeg2000 -frames:v 1 -q:v $bitrate images/JPG2000/bitrate-$((4-$j))/$i.jp2
+    ffmpeg -nostdin -y -i images/JPG2000/bitrate-$((4-$j))/$i.jp2 -codec png -pix_fmt rgb24 images/JPG2000/bitrate-$((4-$j))/$i.png
+  done
+  # AV1
+  for j in {0..3};do
+    let bitrate=$(($j * (40 / 3) + 1))
+    mkdir images/AV1/bitrate-$((4-$j)) 2> /dev/null
+    ffmpeg -nostdin -y -i images/References/$i.png -c:v libx264rgb -pix_fmt rgb24 -crf $bitrate images/AV1/bitrate-$((4-$j))/$i.mp4
+    ffmpeg -nostdin -y -i images/AV1/bitrate-$((4-$j))/$i.mp4 -codec png -pix_fmt rgb24 images/AV1/bitrate-$((4-$j))/$i.png
+  done
 done
