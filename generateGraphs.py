0cf64d24d052f17a416581c43047cafb0fcbbb87
AmrMorgado <angelo.morgado@ubi.pt>
2023-03-14 10:55:47 +0000

    code refactoring part 2 -_-

--- generateGraphs.py
+++ generateGraphs.py
@@ -4,22 +4,19 @@ import numpy as np
 import os
 from tqdm import tqdm
 
-RESULTS_DIR = {}
-CODECS = []
-
 def create_dir():
     if not os.path.exists('graphs'):
         os.mkdir('graphs')
         for metric in objective_metrics:
             os.mkdir(f'graphs/{metric}')
 
-def create_graph(file_path, results_list):
+def create_graph(file_path, results_list, codecs):
     # Reset plot
     plt.clf()
 
     X = [i for i in range(len(results_list[0]))]
     for i,codec_results in enumerate(results_list):
-        plt.plot(X, codec_results, label=CODECS[i])
+        plt.plot(X, codec_results, label=codecs[i])
     
     plt.xlabel("Bitrate")
     plt.ylabel(f"{file_path.split('/')[1]} score")
@@ -33,7 +30,7 @@ if __name__ == '__main__':
     objective_metrics = os.listdir('objective_results')
     codecs_dir = os.listdir(f'objective_results/{objective_metrics[0]}')
     bitrates = list(pd.read_csv(f'objective_results/{objective_metrics[0]}/{codecs_dir[0]}', index_col=0).index)
-    CODECS = [codec.split('-')[0] for codec in codecs_dir]
+    codecs = [codec.split('-')[0] for codec in codecs_dir]
     create_dir()
 
     for metric in tqdm(objective_metrics,desc='Metrics'):
@@ -47,4 +44,4 @@ if __name__ == '__main__':
             for codec_df in df_list:
                 image_results.append(list(codec_df[f'Image {i+1}'])) # This list contains the bitrates values for each codec
             
-            create_graph(f'graphs/{metric}/Image{i + 1}.png', image_results)   
+            create_graph(f'graphs/{metric}/Image{i + 1}.png', image_results, codecs)   
