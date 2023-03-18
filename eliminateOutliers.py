b04141e09baa1380767672abb97ed3100ec63b36
duarte.a.arribas@gmail.com <duarte.a.arribas@gmail.com>
2023-03-18 16:12:47 +0000

    calma

--- eliminateOutliers.py
+++ eliminateOutliers.py
@@ -1,19 +1,36 @@
 import pandas as pd
 import os
 import shutil
+import numpy as np
 
-def remove_user(user):
-    shutil.rmtree('subjective_results/' + user)
 
-def check_user(user, mos_df):
-    user_df = [pd.read_csv(f'{user}/{codec}', index_col=0) for codec in os.listdir(user)]
-    # TODO: Encontrar função do pandas ou outra coisa qualquer para verificar se o user é outlier (user_df e mos_df são listas de dataframes, cada uma com um dataframe de cada codec)
+SUBJECTIVE_RESULTS_DIR = "subjective_results"
 
-if __name__ == '__main__':
-    codec_list = [codec.split('Results')[0].upper() for codec in os.listdir('mos_results')]
-    mos_df = [pd.read_csv(f'mos_results/{codec}', index_col=0) for codec in os.listdir('mos_results')]
-    print(codec_list)
+jpgArrayResults     = pd.DataFrame(np.zeros((5,5)), index = ["bitrate-1", "bitrate-2", "bitrate-3", "bitrate-4","Image Reference"],columns = ["Image 1","Image 2","Image 3","Image 4","Image 5"])
+jpg2000ArrayResults = pd.DataFrame(np.zeros((5,5)), index = ["bitrate-1", "bitrate-2", "bitrate-3", "bitrate-4","Image Reference"],columns = ["Image 1","Image 2","Image 3","Image 4","Image 5"])
+av1ArrayResults     = pd.DataFrame(np.zeros((5,5)), index = ["bitrate-1", "bitrate-2", "bitrate-3", "bitrate-4","Image Reference"],columns = ["Image 1","Image 2","Image 3","Image 4","Image 5"])
 
-    # Go through the users
-    for user in os.listdir('subjective_results'):
-        check_user('subjective_results/' + user, mos_df)
+def generateArrayResults():
+  for userDir in os.listdir(SUBJECTIVE_RESULTS_DIR):
+    for result in os.listdir(os.path.join(SUBJECTIVE_RESULTS_DIR,userDir)):
+      if result == "jpgResults.csv":
+        temp = pd.read_csv(os.path.join(os.path.join(SUBJECTIVE_RESULTS_DIR,userDir),result),index_col=0)
+        for i in range(5):
+          for j in range(5):
+          
+        
+        jpgArrayResults
+      if result == "jpg2000Results.csv":
+        temp = pd.read_csv(os.path.join(os.path.join(SUBJECTIVE_RESULTS_DIR,userDir),result),index_col=0)
+      if result == "av1Results.csv":
+        temp = pd.read_csv(os.path.join(os.path.join(SUBJECTIVE_RESULTS_DIR,userDir),result),index_col=0)
+        print(temp)
+
+def removeUser(user):
+  shutil.rmtree(f"subjective_results/{user}")
+
+def main():
+  generateArrayResults()
+  
+if __name__ == "__main__":
+  main()
