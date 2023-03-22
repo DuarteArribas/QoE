01f87fd607989933e4e71ebfea6d9d92f7f2199e
duarte.a.arribas@gmail.com <duarte.a.arribas@gmail.com>
2023-03-22 01:59:35 +0000

    fix SI

--- computeSI.py
+++ computeSI.py
@@ -25,14 +25,13 @@ def eotfSdr(value):
   return a * np.power(np.maximum(value + b,0),gamma)
 
 def oetf(value):
+  m1 = 0.1593017578125
   m2 = 78.84375
-  n = 0.1593017578125
   c1 = 0.8359375
   c2 = 18.8515625
   c3 = 18.6875
-  lm1 = np.power(10000.0, n)
-  lm2 = np.power(value, n)
-  return np.power((c1 * lm1 + c2 * lm2) / (lm1 + c3 * lm2),m2)
+  Y = value / 10000
+  return np.power((c1 + c2 * np.power(Y,m1)) / (1 + c3 * np.power(Y,m1)),m2)
   
 def denormalize(value,b):
   return float(value * (2 ** b - 1))
@@ -56,8 +55,6 @@ for image in os.listdir("images/InitialReferences"):
   si     = np.array(result).std()
   imagesDict[image] = denormalize(si,32)
   
-
-my_dict = {'a': 10, 'c': 5, 'b': 20}
 print("== SI of the images ==\n")
 print(imagesDict)
 print("\n\n")
